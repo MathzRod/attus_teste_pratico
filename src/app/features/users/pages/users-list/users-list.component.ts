f56e34db610f7dc5272289088263ee91828aab0f
Matheus Rodrigues de Camargo <mathzrodrsocial@gmail.com>
2026-05-14 14:50:50 -0300

    feat: connect users list to mocked service

--- src/app/features/users/pages/users-list/users-list.component.ts
+++ src/app/features/users/pages/users-list/users-list.component.ts
@@ -1,8 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { catchError, finalize, of } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { UsersService } from '../../services/users.service';
+import { UsersStore } from '../../store/users.store';
 
 @Component({
   selector: 'app-users-list',
@@ -16,12 +19,37 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './users-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersListComponent {
-  readonly users = [
-    {
-      id: 1,
-      name: 'Giana Sandrini',
-      email: 'giana@attornatus.com.br',
-    },
-  ];
+export class UsersListComponent implements OnInit {
+  private readonly usersService = inject(UsersService)
+  private readonly usersStore = inject(UsersStore);
+  private readonly destroyRef = inject(DestroyRef);
+
+  readonly loading = signal(false);
+
+  readonly error = signal<string | null>(null);
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  private loadUsers(): void {
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.usersService
+      .getUsers()
+      .pipe(
+        catchError(() => {
+          this.error.set('Não foi possível carregar os usuários.');
+          return of([]);
+        }),
+        finalize(() => {
+          this.loading.set(false);
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((users) => {
+        this.usersStore.setUsers(users);
+      });
+  }
 }
